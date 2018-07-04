b53dacf4bfd7f6b0ceaa26bdc256e88d3473066c
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2018-07-05 01:19:32 +0530

    login form css updated

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,10 +3,12 @@ import { SignupComponent } from "./signup/signup.component";
 import { ListingComponent } from "./listing/listing.component";
 import { AddlistingComponent } from "./addlisting/addlisting.component";
 import { HomeComponent } from "./home/home.component";
+import { SigninComponent } from "./signin/signin.component";
 
 
 const APP_ROUTES: Routes = [
     { path: '', component: HomeComponent },
+    { path: 'signin', component: SigninComponent},
     { path: 'signup', component: SignupComponent },
     {
         path: 'listing', component: ListingComponent, children: [

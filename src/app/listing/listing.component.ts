f07e03a8f9db566ee6d4b484bbb70f865508a937
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2018-07-06 01:47:40 +0530

    routing and checks added

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {Router, ActivatedRoute} from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { ListingService } from '../listing.service';
 import { Listing } from '../listing.model';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-listing',
@@ -10,24 +11,42 @@ import { Listing } from '../listing.model';
 })
 export class ListingComponent implements OnInit {
 
-  listings:any[] =[];
+  listings: any[] = [];
 
-  constructor(private router: ActivatedRoute,private route: Router,private listingservice: ListingService) { }
+  constructor(private router: ActivatedRoute, private route: Router, private listingservice: ListingService, private auth: AuthService) { }
 
   ngOnInit() {
-      this.listingservice.getlisting()
+
+    this.auth.isloggedin()
+      .subscribe(data => {
+        if (!JSON.parse(data["_body"]).done) {
+          this.route.navigate(['']);
+        }
+      })
+
+    this.listingservice.getlisting()
       .subscribe(data => {
         console.log(data["_body"]);
-        this.listings = JSON.parse(data["_body"])   
+        this.listings = JSON.parse(data["_body"])
         console.log(this.listings);
-             
+
       })
   }
 
-  addlisting() {
+  addListingPage() {
     console.log("working");
-    
-    this.route.navigate(['add'], { relativeTo: this.router});
- }
+    this.route.navigate(['add'], { relativeTo: this.router });
+  }
+
+  displayListingPage() {
+    this.route.navigate(['listing']);
+  }
+
+  logout() {
+    this.auth.logout()
+      .subscribe(data => {
+        this.route.navigate(['']);
+      })
+  }
 
 }

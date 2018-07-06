e03a5d07c83aa8fa76d63b143ce77a6ae0ae4e4a
ankitbhardwaj78 <bhardwajankit438@gmail.com>
2018-07-06 17:47:56 +0530

    book detail css  added

--- src/app/bookdetail/bookdetail.component.ts
+++ src/app/bookdetail/bookdetail.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-bookdetail',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bookdetail.component.css']
 })
 export class BookdetailComponent implements OnInit {
+  myForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.myForm = new FormGroup({
+      message: new FormControl(null, Validators.required)
+  });
+  }
+
+  onSubmit(message){
+    console.log("in submit");
+    
   }
 
 }
